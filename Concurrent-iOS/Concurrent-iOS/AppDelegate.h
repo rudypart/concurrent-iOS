4e337f344fb72bb195f81923d13dee6e10ef4ae1
joey_qi <joey_qi@Wolianw-iOS-6deiMac.local>
2017-09-29 15:39:18 +0800

    添加项目！

---
+++ Concurrent-iOS/Concurrent-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Concurrent-iOS
+//
+//  Created by joey_qi on 2017/9/29.
+//  Copyright © 2017年 joey_qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Concurrent-iOS/Concurrent-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Concurrent-iOS
+//
+//  Created by joey_qi on 2017/9/29.
+//  Copyright © 2017年 joey_qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
